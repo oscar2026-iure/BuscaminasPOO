475d6e9ee13a568980a4997fa8bbc73880bd978f
Jack-ez <jzuquilanda@est.ups.edu.ec>
2026-06-05 00:39:44 -0500

    Se importo la clase Main
    El punto de entrada de la aplicación. Su función principal es crear una instancia del controlador del juego.

---
+++ BuscaminasPOO/src/Main.java
@@ -0,0 +1,12 @@
+import controller.JuegoController;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JuegoController controller =
+                new JuegoController();
+
+        controller.iniciar();
+    }
+}
